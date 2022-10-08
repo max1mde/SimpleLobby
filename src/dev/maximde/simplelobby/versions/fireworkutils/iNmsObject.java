47ca98ef0c4fd58d73b7c420ac121fa8c0eb1a04
Maxim <114857048+JavaDevMC@users.noreply.github.com>
2022-10-08 18:36:42 +0200

    Delete src/dev/maximde/simplelobby directory

--- src/dev/maximde/simplelobby/versions/fireworkutils/iNmsObject.java
+++
@@ -1,9 +0,0 @@
-package dev.maximde.simplelobby.versions.fireworkutils;
-
-public interface iNmsObject {
-
-    /**
-     * Build
-     */
-    public Object build()throws Exception;
-}
--- src/dev/maximde/simplelobby/versions/scoreboard/ScoreboardUtil.java
+++
@@ -1,8 +0,0 @@
-package dev.maximde.simplelobby.versions.scoreboard;
-
-import org.bukkit.entity.Player;
-
-public interface ScoreboardUtil {
-
-	public void setScoreboard(Player p, String title, String... lines);
-}
